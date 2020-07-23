7d606b75277c6dbebdeab7e0fdd790ba17eff6e4
logicOnAbstractions <logiconabstractions@gmail.com>
2020-07-23 11:05:00 -0400

    ran git submodule update after updating the remote of the sub.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from sub_submodule_demo.submain import get_version
 
 if __name__ == '__main__':
-    print(f"hello world! now to hear from the submodule:{get_version()}")
+    print(f"hello world!  now to hear from the submodule: {get_version()}")
